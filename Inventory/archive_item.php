a1b091be3523887113ad2ab6968e2a0436f0e4c3
ichan3619 <eightanronda@gmail.com>
2025-05-06 23:38:04 +0800

    Merging fixed

--- Inventory/archive_item.php
+++ Inventory/archive_item.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include '../Config/database.php';
 
@@ -34,40 +33,3 @@ try {
     echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
 }
 ?>
-=======
-<?php
-include '../Config/database.php';
-
-$data = json_decode(file_get_contents("php://input"), true);
-$invID = $data['inventory_no'] ?? '';
-
-
-if (empty($invID)) {
-    echo json_encode(['status' => 'error', 'message' => 'Missing inventory ID']);
-    exit;
-}
-
-try {
-    $conn->beginTransaction();
-
-    // Move item to Archiveinv
-    $insertSql = "INSERT INTO Archiveinv (AinvName, AinvDescription, AinvCategory, AinvDosage, AitemQuantity, AinvSupplyDate, AinvExpiryDate)
-                  SELECT invName, invDescription, invCategory, invDosage, itemQuantity, invSupplyDate, invExpiryDate
-                  FROM inventory WHERE invID = :invID";
-    $stmtInsert = $conn->prepare($insertSql);
-    $stmtInsert->execute([':invID' => $invID]);
-
-    // Delete from inventory
-    $deleteSql = "DELETE FROM inventory WHERE invID = :invID";
-    $stmtDelete = $conn->prepare($deleteSql);
-    $stmtDelete->execute([':invID' => $invID]);
-
-    $conn->commit();
-
-    echo json_encode(['status' => 'success']);
-} catch (PDOException $e) {
-    $conn->rollBack();
-    echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
-}
-?>
->>>>>>> 9af54f3f564a4b70b04f1491995f5037957eac8b
